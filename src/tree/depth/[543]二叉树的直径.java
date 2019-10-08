015cd65e2ddf987318dc0f303f33fc31a77020f3
Noahhhhha <38214421+Wxs-ggbang@users.noreply.github.com>
2019-10-08 17:16:06 +0800

    2019.10.8

---
+++ src/tree/depth/[543]二叉树的直径.java
@@ -0,0 +1,48 @@
+package tree.depth;//给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过根结点。
+//
+// 示例 : 
+//给定二叉树 
+//
+// 
+//          1
+//         / \
+//        2   3
+//       / \     
+//      4   5    
+// 
+//
+// 返回 3, 它的长度是路径 [4,2,1,3] 或者 [5,2,1,3]。 
+//
+// 注意：两结点之间的路径长度是以它们之间边的数目表示。 
+// Related Topics 树
+
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    /**
+     * 左右层数相加即可
+     */
+    int sum = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return sum;
+    }
+    private int dfs(TreeNode root){
+        if (root == null) return 0;
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        sum = Math.max(left + right, sum);
+        return Math.max(left, right) + 1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
