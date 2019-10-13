22325c5a28940da359341bd353e593470f932578
Noahhhhha <38214421+Wxs-ggbang@users.noreply.github.com>
2019-10-13 13:58:13 +0800

    2019.10.13

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # leetcode
-## -src
-### --bs                        二分查找 binary search
-### --dp                        动态规划 dynamic program
-### --linkedList                链表
-### --map                       map字典法
-### --math                      有关数字反转处理或者数学方面
-### --stack                     栈
-### --sw                        滑动窗口 sliding window
-### --tree                      树
-#### ---bst                     二叉搜索树性质应用
-#### ---depth                   二叉树深度问题
-#### ---doubleRecursion         树双递归问题
+    用java语言写leetcode中的一些题，后来觉的方便，把数据结构的一些基本知识也在往这上面总结。
+## src
+- <font color=#00ffff size=72> basic </font>                     写一些基础的算法，如快排
+   - sort                   排序
+- bs                        二分查找 binary search
+- dp                        动态规划 dynamic program
+- linkedList                链表
+- map                       map字典法
+- math                      有关数字反转处理或者数学方面
+- stack                     栈
+- sw                        滑动窗口 sliding window
+- tree                      树
+   - bst                     二叉搜索树性质应用
+   - depth                   二叉树深度问题
+   - doubleRecursion         树双递归问题
---
+++ src/basic/sort/QuickSort.java
@@ -0,0 +1,65 @@
+package sort;
+
+import java.util.Arrays;
+
+/**
+ * Created by Noahhhhha
+ * on 2019/10/13 12:03.
+ */
+public class QuickSort {
+    public static void main(String[] args) {
+        // 给出无序数组
+        int[] arr = {72, 6, 57, 88, 60, 42, 83, 73, 48, 85};
+        // 输出无序数组
+        System.out.println(Arrays.toString(arr));
+        // 快速排序 (对于调用者来说，越方便越好)
+        quickSort(arr);
+        // 输出有序数组
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void quickSort(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        quickSort(arr,start,end);
+    }
+
+    private static void quickSort(int[] arr, int start, int end) {
+        // 分区操作，也是具体连续比较的操作，返回分区界限索引
+        if (start < end){
+            int index = partition(arr,start,end);
+            // 对左分区进行排序
+            quickSort(arr, start, index - 1);
+            // 对右分区进行排序
+            quickSort(arr, index + 1, end);
+        }
+    }
+
+    private static int partition(int[] arr, int start, int end) {
+        // 指定左指针i和右指针j
+        int i = start, j = end;
+        // 将第一个数作为基准值。挖坑
+        int x = arr[start];
+        // 使用循环实现分区操作
+        while (i < j){
+            // 1、从右向左不停的移动j，直到找到第一个小于基准值的arr[j]
+            while (i < j && arr[j] > x) j--;
+            // 2、将右侧找到小于基准值的数加入到左边的（坑）位置，左指针向中间移动一个位置i++
+            if (i < j){ // 防止等于的情况
+                arr[i] = arr[j]; // 此时j指向一个大于基准值的数
+                i++; // i本来指向挖掉的坑（基准值），此处从坑中跳出
+            }
+            // 3、从左向右移动i，找到第一个大于基准值的arr[i]
+            while (i < j && arr[i] < x) i++;
+            // 4、将左侧找到大于基准值的数加入到右边的（坑）位置，右指针向中间移动一个位置j--
+            if (i < j){
+                arr[j] = arr[i];
+                j--; // j从坑中跳出去
+            }
+        }
+        // 使用基准值填坑，这就是基准值的最终位置，此时i == j；arr[j] = x;
+        arr[i] = x;
+        // 返回基准值的位置索引 i == j
+        return i;
+    }
+}
