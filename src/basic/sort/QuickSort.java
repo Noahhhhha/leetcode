86157488ad3f9a3fad3b97ee5789324a22bffea0
Noahhhhha <38214421+Wxs-ggbang@users.noreply.github.com>
2019-10-13 14:12:16 +0800

    2019.10.13

--- src/basic/sort/QuickSort.java
+++ src/basic/sort/QuickSort.java
@@ -26,7 +26,7 @@ public class QuickSort {
 
     private static void quickSort(int[] arr, int start, int end) {
         // 分区操作，也是具体连续比较的操作，返回分区界限索引
-        if (start < end){
+        if (start < end){ // 一直分治到一组只剩一个数，start == end 时 结束
             int index = partition(arr,start,end);
             // 对左分区进行排序
             quickSort(arr, start, index - 1);
@@ -41,12 +41,12 @@ public class QuickSort {
         // 将第一个数作为基准值。挖坑
         int x = arr[start];
         // 使用循环实现分区操作
-        while (i < j){
+        while (i < j){ // 当他们俩相等的时候，就都移动完了
             // 1、从右向左不停的移动j，直到找到第一个小于基准值的arr[j]
             while (i < j && arr[j] > x) j--;
             // 2、将右侧找到小于基准值的数加入到左边的（坑）位置，左指针向中间移动一个位置i++
-            if (i < j){ // 防止等于的情况
-                arr[i] = arr[j]; // 此时j指向一个大于基准值的数
+            if (i < j){ // 如果等于的话，就没必要移动了
+                arr[i] = arr[j]; // 此时j指向一个大于基准值的数，放置到i指向的坑中
                 i++; // i本来指向挖掉的坑（基准值），此处从坑中跳出
             }
             // 3、从左向右移动i，找到第一个大于基准值的arr[i]
