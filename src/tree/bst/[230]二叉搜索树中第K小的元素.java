0bab5900dfced6f032b6f95017795bb0b8d686ce
Noahhhhha <38214421+Wxs-ggbang@users.noreply.github.com>
2019-10-28 20:08:40 +0800

    2019.10.28

---
+++ src/tree/bst/[230]二叉搜索树中第K小的元素.java
@@ -0,0 +1,59 @@
+//给定一个二叉搜索树，编写一个函数 kthSmallest 来查找其中第 k 个最小的元素。 
+//
+// 说明： 
+//你可以假设 k 总是有效的，1 ≤ k ≤ 二叉搜索树元素个数。 
+//
+// 示例 1: 
+//
+// 输入: root = [3,1,4,null,2], k = 1
+//   3
+//  / \
+// 1   4
+//  \
+//   2
+//输出: 1 
+//
+// 示例 2: 
+//
+// 输入: root = [5,3,6,2,4,null,null,1], k = 3
+//       5
+//      / \
+//     3   6
+//    / \
+//   2   4
+//  /
+// 1
+//输出: 3 
+//
+// 进阶： 
+//如果二叉搜索树经常被修改（插入/删除操作）并且你需要频繁地查找第 k 小的值，你将如何优化 kthSmallest 函数？ 
+// Related Topics 树 二分查找
+
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    private int res = 0, i;
+    public int kthSmallest(TreeNode root, int k) {
+        // 二叉搜索树，中序遍历出来就是从小到大排序好的
+        i = k;
+        helper(root);
+        return res;
+    }
+    private void helper(TreeNode root){
+        if (root == null || i == 0) return;
+        helper(root.left);
+        if(--i == 0) res = root.val;
+        helper(root.right);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
