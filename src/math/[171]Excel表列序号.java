d13ef8e13b62c205806607f4ef826b028ba8fb0e
Noahhhhha <38214421+Wxs-ggbang@users.noreply.github.com>
2019-11-11 15:21:16 +0800

    2019.11.11 70

---
+++ src/math/[171]Excel表列序号.java
@@ -0,0 +1,54 @@
+//给定一个Excel表格中的列名称，返回其相应的列序号。 
+//
+// 例如， 
+//
+//     A -> 1
+//    B -> 2
+//    C -> 3
+//    ...
+//    Z -> 26
+//    AA -> 27
+//    AB -> 28 
+//    ...
+// 
+//
+// 示例 1: 
+//
+// 输入: "A"
+//输出: 1
+// 
+//
+// 示例 2: 
+//
+// 输入: "AB"
+//输出: 28
+// 
+//
+// 示例 3: 
+//
+// 输入: "ZY"
+//输出: 701 
+//
+// 致谢： 
+//特别感谢 @ts 添加此问题并创建所有测试用例。 
+// Related Topics 数学
+
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int titleToNumber(String s) {
+        /**
+         * 重点：
+         * 1、对编码字符的加减法
+         * 2、每次循环都要将原来的答案*26，相当于左移了一位
+         */
+        int ans = 0;
+        for (int i = 0; i < s.length(); i++){
+            int number = s.charAt(i) - 'A' + 1;
+            ans = ans * 26 + number;
+        }
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
